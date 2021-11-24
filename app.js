3a99e4cccc6e7ef45b430457f0bc2607692401c7
Rácz Viktor <raczviktor0220@gmail.com>
2021-11-24 11:45:59 +0100

    Add users Guide

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log(
+    `$ todo
+
+    Parancssori Todo applikáció
+    =============================
+    
+    Parancssori argumentumok:
+        -l   Kilistázza a feladatokat
+        -a   Új feladatot ad hozzá
+        -r   Eltávolít egy feladatot
+        -c   Teljesít egy feladatot`
+)
