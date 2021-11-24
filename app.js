4a27f4c3eaf4bc98941a1e454d13f384413b0e24
Rácz Viktor <raczviktor0220@gmail.com>
2021-11-24 16:53:44 +0100

    Refactoring

--- app.js
+++ app.js
@@ -4,30 +4,28 @@ const args = process.argv;
 
 const appRoot = args[1].slice(0, -6);
 
-
 const printUserGuide = () => {
-    const userGuide = fs.readFileSync(appRoot + 'userGuide.txt').toString();
+
+    const userGuide = fs.readFileSync(appRoot + 'userGuide.txt')
+        .toString();
 
     console.log(userGuide);
 };
 
 
 const printList = () => {
-    let todoList = [];
 
-    const rawList = fs.readFileSync(appRoot + 'todos.txt').toString();
+    let todoList = fs.readFileSync(appRoot + 'todos.txt')
+        .toString()
+        .split('\n')
+        .filter(content => content !== '');
 
-    todoList = rawList.split('\n');
-
-    let filteredList = todoList.filter(content => content !== '');
-        
-      if (filteredList.length === 0) {
+    if (todoList.length === 0) {
         console.log('Nincs mára tennivalód! :)');
-      }
-   
+    }
 
-    for (let i = 0; i < filteredList.length; i++) {
-        console.log(`${(i+1)} - ${filteredList[i]}`);
+    for (let i = 0; i < todoList.length; i++) {
+        console.log(`${(i + 1)} - ${todoList[i]}`);
     }
 };
 
