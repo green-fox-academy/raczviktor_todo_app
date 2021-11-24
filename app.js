5fee2d1868c3908dfebdaede0615727513e1c54a
Rácz Viktor <raczviktor0220@gmail.com>
2021-11-24 18:11:31 +0100

    Add new fuction: add new todo. Add error handle for this.

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const args = process.argv;
 
 const appRoot = args[1].slice(0, -6);
 
+
+
+
+
+
+
+
 const printUserGuide = () => {
 
     const userGuide = fs.readFileSync(appRoot + 'userGuide.txt')
@@ -15,23 +22,36 @@ const printUserGuide = () => {
 
 const printList = () => {
 
-    let todoList = fs.readFileSync(appRoot + 'todos.txt')
+    let cleanList = fs.readFileSync(appRoot + 'todos.txt')
         .toString()
         .split('\n')
         .filter(content => content !== '');
 
-    if (todoList.length === 0) {
+    if (cleanList.length === 0) {
         console.log('Nincs mára tennivalód! :)');
     }
 
-    for (let i = 0; i < todoList.length; i++) {
-        console.log(`${(i + 1)} - ${todoList[i]}`);
+    for (let i = 0; i < cleanList.length; i++) {
+        console.log(`${(i + 1)} - ${cleanList[i]}`);
     }
 };
 
 
+const addTodo = () => {
+
+    const newTodo = args[3];
+
+    let rawList = fs.readFileSync(appRoot + 'todos.txt').toString();
+
+    newTodo ? fs.writeFileSync(appRoot + 'todos.txt', rawList + '\n' + newTodo )
+        : console.log('Nem lehetséges új feladat hozzáadása: nincs megadva a feladat!');
+};
+
+
+
 switch (args[2]) {
     case '-l': { printList(); break; }
+    case '-a': { addTodo(); break; }
     default: {
         printUserGuide();
     }
